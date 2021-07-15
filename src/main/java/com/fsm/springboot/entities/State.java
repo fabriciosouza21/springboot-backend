e18196f8f5856fc7b2d1534ae21ea91c3ca35c38
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-15 17:03:55 -0300

    adding annotation entity em State

--- src/main/java/com/fsm/springboot/entities/State.java
+++ src/main/java/com/fsm/springboot/entities/State.java
@@ -3,10 +3,12 @@ package com.fsm.springboot.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class State  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
