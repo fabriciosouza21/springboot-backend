63c2d8262a72619e65db0729ac9dc9e57669f76e
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-15 16:51:05 -0300

    creating the state class

--- src/main/java/com/fsm/springboot/entities/State.java
+++ src/main/java/com/fsm/springboot/entities/State.java
@@ -1,5 +1,61 @@
 package com.fsm.springboot.entities;
 
-public class State {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class State  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	private String nome;
+
+	public State() {
+	
+	}
+
+	public State(String id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
