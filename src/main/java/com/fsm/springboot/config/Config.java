e4763a3b3db2cc769eedaf5b2824bd892bf7a1c0
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-16 20:26:50 -0300

    creation in config, initial instantiation

---
+++ src/main/java/com/fsm/springboot/config/Config.java
@@ -0,0 +1,26 @@
+package com.fsm.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.fsm.springboot.entities.State;
+import com.fsm.springboot.repositories.StateRepository;
+
+@Configuration
+public class Config implements CommandLineRunner{
+	@Autowired
+	StateRepository stateRepository;
+	
+	@Autowired
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		State para = new State(null, "para");
+		stateRepository.save(para);
+		
+		
+	}
+
+}
