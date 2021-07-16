ce2668ba7ab816dc9e3f0e15fbeb999a4f79309f
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-16 20:28:33 -0300

    creation city repository

---
+++ src/main/java/com/fsm/springboot/repositories/CityRepository.java
@@ -0,0 +1,9 @@
+package com.fsm.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fsm.springboot.entities.City;
+
+public interface CityRepository extends JpaRepository<City, String>{
+
+}
