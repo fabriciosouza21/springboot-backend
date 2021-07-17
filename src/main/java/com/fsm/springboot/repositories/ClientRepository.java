419d281550f196d512e0a8bd844a82d511018e65
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-17 18:45:16 -0300

    created client repository

---
+++ src/main/java/com/fsm/springboot/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.fsm.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fsm.springboot.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, String>{
+
+}
