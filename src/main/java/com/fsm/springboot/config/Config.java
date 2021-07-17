12622dd939c898a2e49de136546acc3552e36739
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-17 18:30:34 -0300

    testing city instatiation

--- src/main/java/com/fsm/springboot/config/Config.java
+++ src/main/java/com/fsm/springboot/config/Config.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.fsm.springboot.entities.Address;
+import com.fsm.springboot.entities.City;
 import com.fsm.springboot.entities.State;
+import com.fsm.springboot.repositories.AddressRepository;
+import com.fsm.springboot.repositories.CityRepository;
 import com.fsm.springboot.repositories.StateRepository;
 
 @Configuration
@@ -12,12 +16,17 @@ public class Config implements CommandLineRunner{
 	@Autowired
 	StateRepository stateRepository;
 	
+	@Autowired
+	CityRepository cityRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		State para = new State(null, "para");
 		stateRepository.save(para);
 		
+		City belem = new City(null, "belem", para);
+		cityRepository.save(belem);
 		
 	}
 
