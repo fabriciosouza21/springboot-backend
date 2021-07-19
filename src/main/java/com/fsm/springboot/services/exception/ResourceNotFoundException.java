dc70296e77b93668051937daa1fb1e041468615a
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-19 18:01:43 -0300

    created Standar erro and mover resourceNotFoundException for services

--- src/main/java/com/fsm/springboot/services/exception/ResourceNotFoundException.java
+++ src/main/java/com/fsm/springboot/services/exception/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public ResourceNotFoundException() {
-		super ("not found file");
+		super("Resource not found");
 	}
-	
+
 }
