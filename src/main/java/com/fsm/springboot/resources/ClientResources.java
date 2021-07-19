7308bf471636b4b4ed9e235470aa69c7f1268713
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-19 16:41:07 -0300

    adding to ClientResources save

--- src/main/java/com/fsm/springboot/resources/ClientResources.java
+++ src/main/java/com/fsm/springboot/resources/ClientResources.java
@@ -1,12 +1,17 @@
 package com.fsm.springboot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fsm.springboot.entities.Client;
 import com.fsm.springboot.services.ClientServices;
@@ -22,6 +27,12 @@ public class ClientResources {
 	public ResponseEntity<List<Client>> findAll() {
 		List<Client> clients = services.findAll();
 		return ResponseEntity.ok().body(clients);
+	}
+	@PostMapping
+	public ResponseEntity<Client> save(@RequestBody Client client){
+		client = services.save(client);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri();
+		return ResponseEntity.created(uri).body(client);
 	}
 
 }
