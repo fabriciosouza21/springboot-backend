a9172fc8bf67db13316fa332c6dafd1af972eb18
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-19 19:11:29 -0300

    adding deleteByid in ClientResources

--- src/main/java/com/fsm/springboot/resources/ClientResources.java
+++ src/main/java/com/fsm/springboot/resources/ClientResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,11 @@ public class ClientResources {
 	public ResponseEntity<Client> findByEmail(@PathVariable String email){
 		Client client = services.findByEmail(email);
 		return ResponseEntity.ok().body(client);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable String id){
+		services.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
