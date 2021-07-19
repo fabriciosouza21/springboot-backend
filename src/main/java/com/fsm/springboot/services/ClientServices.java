2fb8a263a7ab6c246f413418c951819ea1b3440d
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-19 15:58:31 -0300

    created client services

---
+++ src/main/java/com/fsm/springboot/services/ClientServices.java
@@ -0,0 +1,21 @@
+package com.fsm.springboot.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fsm.springboot.entities.Client;
+import com.fsm.springboot.repositories.ClientRepository;
+
+@Service
+public class ClientServices {
+	
+	@Autowired
+	ClientRepository repository;
+	
+	public List<Client> findAll(){
+		return repository.findAll();
+	}	
+	
+}
