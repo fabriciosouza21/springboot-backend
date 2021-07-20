ffbd9a1c9ac3c4bc6f434d821c876a01aec891f5
fabricio21777 <fabriciosouza21777@gmail.com>
2021-07-20 18:53:25 -0300

    adding update in client

--- src/main/java/com/fsm/springboot/services/ClientServices.java
+++ src/main/java/com/fsm/springboot/services/ClientServices.java
@@ -1,6 +1,7 @@
 package com.fsm.springboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -44,6 +45,24 @@ public class ClientServices {
 		}catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException();
 		}
+		
+	}
+	
+	public void update(Client clientModify) {
+		try {
+			Optional<Client> clientSource = repository.findById(clientModify.getId());
+			updateData(clientModify, clientSource.get());
+			repository.save(clientSource.get());
+		}
+		catch(RuntimeException e){
+			e.printStackTrace();
+		}
+	}
+
+	private void updateData(Client clientModify,Client clientSource) {
+		clientSource.setCpfOuCnpj(clientModify.getCpfOuCnpj());
+		clientSource.setEmail(clientModify.getEmail());
+		clientSource.setName(clientModify.getName());
 		
 	}
 }
