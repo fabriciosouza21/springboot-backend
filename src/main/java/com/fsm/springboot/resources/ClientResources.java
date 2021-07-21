069adcabb23eecafb3a33901b6652ebe5183381a
fabricio21777 <fabriciosouza21777@gmail.com>
2021-07-21 17:39:56 -0300

    adding in ClientResources update

--- src/main/java/com/fsm/springboot/resources/ClientResources.java
+++ src/main/java/com/fsm/springboot/resources/ClientResources.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,11 @@ public class ClientResources {
 		services.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
+	@PutMapping(value = "/update")
+	public ResponseEntity<Void> update(@RequestBody Client client){
+		services.update(client);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
