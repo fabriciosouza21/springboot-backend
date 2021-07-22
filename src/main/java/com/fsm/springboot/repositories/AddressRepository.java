0d3dfb779fffc3f8fe9f6f7cdd32011a7a20a986
fabricio21777 <fabriciosouza21777@gmail.com>
2021-07-22 15:48:45 -0300

    created findByCity in AddressRepository

--- src/main/java/com/fsm/springboot/repositories/AddressRepository.java
+++ src/main/java/com/fsm/springboot/repositories/AddressRepository.java
@@ -1,9 +1,14 @@
 package com.fsm.springboot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fsm.springboot.entities.Address;
+import com.fsm.springboot.entities.City;
 
 public interface AddressRepository extends JpaRepository<Address, String>{
-
+	
+	List<Address> findByCity(City city);
+	
 }
