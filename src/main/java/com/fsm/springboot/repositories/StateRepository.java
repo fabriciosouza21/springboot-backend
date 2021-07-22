8f7c591b1aa85789165b73b766750a9fe63c6a97
fabricio21777 <fabriciosouza21777@gmail.com>
2021-07-22 16:03:41 -0300

    created findByNome in StateRepository

--- src/main/java/com/fsm/springboot/repositories/StateRepository.java
+++ src/main/java/com/fsm/springboot/repositories/StateRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fsm.springboot.entities.State;
 
 public interface StateRepository extends JpaRepository<State, String>{
-
+	
+	State findByNome(String nome);
+	
 }
